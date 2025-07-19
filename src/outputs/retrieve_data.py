ad9a8e5e15b03741c57eaaf3f929510aeb74687b
Marcelo Zevallos Gavidia <158067068+MarceloZevallos@users.noreply.github.com>
2025-07-19 15:57:29 -0400

    Update retrieve_data.py
    Alignement des commentaires.

--- src/outputs/retrieve_data.py
+++ src/outputs/retrieve_data.py
@@ -66,16 +66,18 @@ def get_z_values(n:int):
             data_list = ast.literal_eval(line.strip())
             
             if isinstance(data_list, list):
-                for data in data_list:                        # Vérifier si le nombre d'objets est supérieur à 0
+                for data in data_list:                        
+                    # Vérifier si le nombre d'objets est supérieur à 0
                     if isinstance(data, dict) and 'nObjects' in data and data['nObjects'] > 0:
                         # Récupérer les objets
                         objects = data['objects']
                         for obj in objects:
-                            # Récupérer la position et la valeur z                                position = obj['position']
+                            # Récupérer la position et la valeur z                                
+                            # position = obj['position']
                             z = position[n] 
                             z_values.append(z)
             else:
-                    # Si le résultat n'est pas une liste, traiter comme un seul dictionnaire
+                # Si le résultat n'est pas une liste, traiter comme un seul dictionnaire
                 if isinstance(data_list, dict) and 'nObjects' in data_list and data_list['nObjects'] > 0:
                     objects = data_list['objects']
                     for obj in objects:
