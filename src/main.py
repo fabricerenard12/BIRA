632b2d0091261e53e10fc4d20516909af66ecca4
Michellen Nguyen <michellenguyen236@gmail.com>
2025-08-14 21:21:23 -0400

    add a GUI for text display

--- src/main.py
+++ src/main.py
@@ -1,6 +1,9 @@
+import multiprocessing
 from scipy.stats import trim_mean
 import speech_to_text
 import numpy as np
+from text_viewer import TextViewer
+from time import sleep
 import utils
 import history as history
 import detector
@@ -30,29 +33,61 @@ def find_angle(coordinated_target_list:np.ndarray, mode: Mode = Mode.FIX_THRESHO
     angle_deg = math.degrees(angle_rad)
     return angle_deg
 
-def main():
+def run_test_motors():
+    while True:
+            try:
+                a = input("Enter angle: ")
+                b = input("Enter motor ID: ")
+                if input("Do you want to enter velocity? (y/N): ").strip().lower() == 'y':
+                    c = input("Enter velocity %: ")
+                else:
+                    c = 80
+                uart.send_data_through_UART(int(a), int(b), int(c))
+                print("Data sent successfully.\n")
+            except ValueError:
+                print("Invalid input. Please enter numeric values.\n\n")           
+
+def run_stt():
+    def run_text_window(self, queue: multiprocessing.Queue):
+        app = TextViewer(queue)
+        app.open()
+        
+    text_queue = multiprocessing.Queue()
+    text_process = multiprocessing.Process(target=run_text_window, args=(text_queue,))
+    text_process.start()
+
+    text_queue.put("Je suis BIRA, le bras robotique de HEKA.")
+    sleep(3)
+    
+    while True:
+        text_queue.put({"text": "Dis moi quelque chose!", "countdown": 10})
+        text = speech_to_text.transcribe_for(10)
+        text_queue.put({"text": f"Tu as dis: \n{text}", "countdown": 10})
+        sleep(10)
+
+def run_bira_sequence(): 
     text = speech_to_text.transcribe_directly()
     print(text)
-    text = 'personne'
     label = utils.string_to_label(text)
-    print(label)
+    print(label)   
 
+def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--weights', type=str, default='../models/yolov8n.pt', help='model.pt path(s)')
     parser.add_argument('--svo', type=str, default=None, help='optional svo file')
     parser.add_argument('--img_size', type=int, default=416, help='inference size (pixels)')
     parser.add_argument('--conf_thres', type=float, default=0.4, help='object confidence threshold')
+    parser.add_argument('--mode', type=str, default="", help='')
     opt = parser.parse_args()
 
-    with torch.no_grad():
-        coordinate_dict = detector.object_detection(60, opt)
-
-        if label not in coordinate_dict:
-            raise ValueError(f"Label {label} not found in coordinate dictionary.")
-        else:
-            for obj_id, positions in coordinate_dict[label].items():
-                angle = find_angle(positions)
-                print(f"Angle for object ID {obj_id} with label {label}: {angle}")
-
+    if opt.mode == "stt":
+        run_stt()
+    elif opt.mode == "cv":
+        pass
+    elif opt.mode == "motors":
+        run_test_motors()
+    else:
+        run_bira_sequence()
+        
 if __name__ == '__main__':
     main()
